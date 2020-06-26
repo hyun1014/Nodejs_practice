5caa2f89bfc4c4e9cf652082a7cbbba90cbd2c74
hyun1014 <ksp70712@gmail.com>
2020-06-26 19:54:01 +0900

    Create 구현 (Express)

--- app.js
+++ app.js
@@ -32,6 +32,47 @@ app.get('/page/:page', (req, res) => {
     });
 });
 
+app.get('/create', (req, res) => {
+    title = 'Create';
+    description = `
+        <form action='/create_process' method='POST'>
+            Title: <input type='text' name='title' placeholder='title'><br>
+            Description:<br>
+            <textarea name='description'>Input description</textarea><br>
+            <input type='submit' value="Submit!">
+        </form>
+    `;
+    control = "Creating...";
+    ret_template = template.make_html(title, fli_template, description, control);
+    res.send(ret_template);
+});
+
+app.post('/create_process', (req, res) => {
+    var body = "";
+    req.on('data', (data) => {
+        body += data;
+
+        if(body.length > 1e6) // 데이터 값이 너무 크면 연결 끊음
+            req.socket.destroy();
+    });
+    req.on('end', (err) => {
+        if(err){
+            console.log(err);
+            throw err;
+        }
+        var post_data = qs.parse(body);
+        console.log(post_data); // post로 받은 값 확인
+        var post_title = path.parse(post_data.title).base;
+        var post_des = post_data.description;
+        fs.writeFile(`./data/${post_title}`, post_des, 'utf-8', (err) => {
+            fli.push(post_title); // 새로운 파일을 File list에 추가
+            fli_template = template.make_list(fli); // List template update
+            res.writeHead(302, {Location: `/page/${post_title}`});
+            res.end();
+        });
+    });
+});
+
 app.listen(PORT, '127.0.0.1', () => {
     console.log("Listening at port 3000...");
 });
