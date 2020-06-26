4cf3b020120964fc3c03754b8b2881dc74a36eb7
hyun1014 <ksp70712@gmail.com>
2020-06-26 18:24:01 +0900

    Delete 구현

--- main.js
+++ main.js
@@ -2,37 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const qs = require('querystring');
 const url = require('url');
-
-var template = {
-    make_html: function(title, list, body, control){
-        return `
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset='utf-8'>
-                <title>nodejs - ${title}</title>
-            </head>
-            <body>
-                <h1><a href='/'>Home</a></h1>
-                ${list}
-                <h2>${title}</h2>
-                ${control}<br /><br />
-                ${body}
-            </body>
-        <html>
-        `
-    },
-    make_list: function(flist){
-        var len = flist.length;
-        var tmp_list = `<ol>
-        `
-        for(var i=0 ; i<len ; i++)
-            tmp_list += `<li><a href="/?id=${flist[i]}">${flist[i]}</a></li>`
-        tmp_list += `</ol>`
-        return tmp_list;
-    }
-}
-
+const template = require('./lib/template.js');
 
 var app = http.createServer(function(req, res){
     var _url = req.url;
@@ -43,9 +13,9 @@ var app = http.createServer(function(req, res){
     var description = undefined;
     var fli = fs.readdirSync(`./data`, 'utf-8'); // 이건 그냥 시험삼아 비동기로
     var fli_template = template.make_list(fli);
-    var control = `<a href="/create">Create post</a>`;
+    var control = `<a href="/create">Create post</a>`; //Create는 기본으로 항상 있음
     var ret_template = undefined;
-
+    // Read Start
     if(pathname=='/'){
         if(queryData.id===undefined){
             title = "Index";
@@ -58,6 +28,9 @@ var app = http.createServer(function(req, res){
         } 
         else{
             title = queryData.id;
+            control += `&nbsp;&nbsp;<a href='/update?id=${title}'>Update</a>&nbsp;&nbsp;
+            <form action='/delete_process' method='POST'><input type='hidden' name='target' value=${title}><input type='submit' value='Delete'></form>
+            `; //Target이 있는 경우 Update, Delete 선택지 추가, Delete는 POST로 처리
             fs.readFile(`./data/${title}`, 'utf-8', (err, data) => {
                 if(err){
                     console.log(err);
@@ -71,6 +44,8 @@ var app = http.createServer(function(req, res){
             });
         }
     }
+    // Read End
+    // Create Start
     else if(pathname=='/create'){
         title = 'Create';
         description = `
@@ -97,6 +72,10 @@ var app = http.createServer(function(req, res){
                     req.socket.destroy();
             });
             req.on('end', (err) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
                 var post_data = qs.parse(body);
                 console.log(post_data); // post로 받은 값 확인
                 var post_title = post_data.title;
@@ -113,6 +92,100 @@ var app = http.createServer(function(req, res){
             res.end();
         }
     }
+    // Create End
+    // Update Start
+    else if(pathname=='/update'){
+        title = `Updating ${queryData.id}`;
+        control = "Updating...";
+        fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, data) => {
+            if(err){
+                console.log(err);
+                throw err;
+            }
+            description = `
+            <form action='/update_process' method='POST'>
+                <input type='hidden' name='origin_title' value=${queryData.id}>
+                Title: <input type='text' name='title' value=${queryData.id}><br>
+                Description:<br>
+                <textarea name='description'>${data}</textarea><br>
+                <input type='submit' value="Submit!">
+            </form>
+            `;
+            ret_template = template.make_html(title, fli_template, description, control);
+            res.writeHead(200);
+            res.write(ret_template);
+            res.end();
+        });
+    }
+    else if(pathname=='/update_process'){
+        if(req.method=='POST'){
+            var body = "";
+            req.on('data', (data) => {
+                body += data;
+
+                if(body.length > 1e6) // 데이터 값이 너무 크면 연결 끊음
+                    req.socket.destroy();
+            });
+            req.on('end', (err) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                var post_data = qs.parse(body);
+                console.log(post_data); // post로 받은 값 확인
+                var post_origin_title = post_data.origin_title;
+                var post_title = post_data.title;
+                var post_des = post_data.description;
+                fs.rename(`data/${post_origin_title}`, `data/${post_title}`, () => {
+                    fs.writeFile(`data/${post_title}`, post_des, 'utf-8', () => {
+                        res.writeHead(302, {Location: `/?id=${post_title}`});
+                        res.end();
+                    });
+                });
+            });
+        }
+        else{
+            console.log("Issue: Illegal access to create_process (GET)");
+            res.writeHead(302, {Location: `/`});
+            res.end();
+        }
+    }
+    // Update End
+    // Delete Start
+    else if(pathname=='/delete_process'){
+        if(req.method=='POST'){
+            var body = "";
+            req.on('data', (data) => {
+                body += data;
+
+                if(body.length > 1e6) // 데이터 값이 너무 크면 연결 끊음
+                    req.socket.destroy();
+            });
+            req.on('end', (err) => {
+                if(err){
+                    console.log(err);
+                    throw err;
+                }
+                var post_data = qs.parse(body);
+                console.log(post_data); // post로 받은 값 확인
+                var post_target = post_data.target;
+                fs.unlink(`data/${post_target}`, () => {
+                    res.writeHead(302, {Location: '/'});
+                    res.end();
+                });
+            });
+        }
+        else{
+            console.log("Issue: Illegal access to create_process (GET)");
+            res.writeHead(302, {Location: `/`});
+            res.end();
+        }
+    }
+    else{
+        res.writeHead(404);
+        res.end("404 not found");
+    }
+    // Delete End
 });
 
 app.listen(3000, '127.0.0.1');
