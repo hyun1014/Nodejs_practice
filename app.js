7cb47cf8fc0b8be707630a7e911586975a26ea0a
hyun1014 <ksp70712@gmail.com>
2020-06-26 21:01:49 +0900

    Used body-parser

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const url = require('url');
 const template = require('./lib/template_express.js');
 const path = require('path');
 const sani_html = require('sanitize-html');
+const body_parser = require('body-parser');
 
 var title = undefined;
 var description = undefined;
@@ -16,6 +17,8 @@ var fli_template = template.make_list(fli);
 var default_control = `<a href="/create">Create post</a>`; //기본 control
 //var target_control = '<a href="/update/${title}">Update this post</a>'; 얘는 안됨
 var ret_template = undefined;
+
+app.use(body_parser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     title = 'Index';
@@ -53,28 +56,14 @@ app.get('/create', (req, res) => {
 });
 
 app.post('/create_process', (req, res) => {
-    var body = "";
-    req.on('data', (data) => {
-        body += data;
-
-        if(body.length > 1e6) // 데이터 값이 너무 크면 연결 끊음
-            req.socket.destroy();
-    });
-    req.on('end', (err) => {
-        if(err){
-            console.log(err);
-            throw err;
-        }
-        var post_data = qs.parse(body);
-        console.log(post_data); // post로 받은 값 확인
-        var post_title = path.parse(post_data.title).base;
-        var post_des = post_data.description;
-        fs.writeFile(`./data/${post_title}`, post_des, 'utf-8', (err) => {
-            fli.push(post_title); // 새로운 파일을 File list에 추가
-            fli_template = template.make_list(fli); // List template update
-            res.writeHead(302, {Location: `/page/${post_title}`});
-            res.end();
-        });
+    var post_data = req.body;
+    var post_title = path.parse(post_data.title).base;
+    var post_des = post_data.description;
+    fs.writeFile(`./data/${post_title}`, post_des, 'utf-8', (err) => {
+        fli.push(post_title); // 새로운 파일을 File list에 추가
+        fli_template = template.make_list(fli); // List template update
+        res.writeHead(302, {Location: `/page/${post_title}`});
+        res.end();
     });
 });
 
@@ -104,74 +93,48 @@ app.get(`/update/:target`, (req, res) => {
 });
 
 app.post('/update_process', (req,res) => {
-    var body = "";
-    req.on('data', (data) => {
-        body += data;
-
-        if(body.length > 1e6) // 데이터 값이 너무 크면 연결 끊음
-            req.socket.destroy();
-    });
-    req.on('end', (err) => {
-        if(err){
-            console.log(err);
-            throw err;
-        }
-        var post_data = qs.parse(body);
-        console.log(post_data); // post로 받은 값 확인
-        var post_origin_title = path.parse(post_data.origin_title).base;
-        var post_title = path.parse(post_data.title).base;
-        var post_des = post_data.description;
-        fs.rename(`data/${post_origin_title}`, `data/${post_title}`, () => {
-            fs.writeFile(`data/${post_title}`, post_des, 'utf-8', () => {
-                // File list update
-                if(post_origin_title != post_title){
-                    var new_fli = [];
-                    var fli_len = fli.length;
-                    for(var i=0 ; i<fli_len ; i++)
-                        if(fli[i] != post_origin_title)
-                            new_fli.push(fli[i]);
-                    new_fli.push(post_title);
-                    fli = new_fli;
-                    fli_template = template.make_list(new_fli);
-                }
-                // File list update done
-                /*
-                Read 부분에서 fs.readdir 이걸 계속 썼으면 이게 필요없다. Async가 이래서 좋은건가
-                */
-                res.redirect(`/page/${post_title}`);
-            });
+    var post_data = req.body;
+    console.log(post_data); // post로 받은 값 확인
+    var post_origin_title = path.parse(post_data.origin_title).base;
+    var post_title = path.parse(post_data.title).base;
+    var post_des = post_data.description;
+    fs.rename(`data/${post_origin_title}`, `data/${post_title}`, () => {
+        fs.writeFile(`data/${post_title}`, post_des, 'utf-8', () => {
+            // File list update
+            if(post_origin_title != post_title){
+                var new_fli = [];
+                var fli_len = fli.length;
+                for(var i=0 ; i<fli_len ; i++)
+                    if(fli[i] != post_origin_title)
+                        new_fli.push(fli[i]);
+                new_fli.push(post_title);
+                fli = new_fli;
+                fli_template = template.make_list(new_fli);
+            }
+            // File list update done
+            /*
+            Read 부분에서 fs.readdir 이걸 계속 썼으면 이게 필요없다. Async가 이래서 좋은건가
+            */
+            res.redirect(`/page/${post_title}`);
         });
     });
 });
 
 app.post('/delete_process', (req, res) => {
-    var body = "";
-    req.on('data', (data) => {
-        body += data;
-
-        if(body.length > 1e6) // 데이터 값이 너무 크면 연결 끊음
-            req.socket.destroy();
-    });
-    req.on('end', (err) => {
-        if(err){
-            console.log(err);
-            throw err;
-        }
-        var post_data = qs.parse(body);
-        console.log(post_data); // post로 받은 값 확인
-        var post_target = path.parse(post_data.target).base;
-        fs.unlink(`data/${post_target}`, () => {
-            // File list update
-            var new_fli = [];
-            var fli_len = fli.length;
-            for(var i=0 ; i<fli_len ; i++)
-                if(fli[i] != post_target)
-                    new_fli.push(fli[i]);
-            fli = new_fli;
-            fli_template = template.make_list(new_fli);
-            // File list update done
-            res.redirect('/');
-        });
+    var post_data = req.body;
+    console.log(post_data); // post로 받은 값 확인
+    var post_target = path.parse(post_data.target).base;
+    fs.unlink(`data/${post_target}`, () => {
+        // File list update
+        var new_fli = [];
+        var fli_len = fli.length;
+        for(var i=0 ; i<fli_len ; i++)
+            if(fli[i] != post_target)
+                new_fli.push(fli[i]);
+        fli = new_fli;
+        fli_template = template.make_list(new_fli);
+        // File list update done
+        res.redirect('/');
     });
 });
 
