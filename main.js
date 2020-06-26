048d03aa3e49d73be07260cbdae7ee2b9992f023
hyun1014 <ksp70712@gmail.com>
2020-06-26 18:43:03 +0900

    Added path, sanitize-html module

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const qs = require('querystring');
 const url = require('url');
 const template = require('./lib/template.js');
+const path = require('path');
+const sani_html = require('sanitize-html');
 
 var app = http.createServer(function(req, res){
     var _url = req.url;
@@ -11,11 +13,11 @@ var app = http.createServer(function(req, res){
     console.log(url.parse(_url, true)); // for test
     var title = undefined;
     var description = undefined;
-    var fli = fs.readdirSync(`./data`, 'utf-8'); // 이건 그냥 시험삼아 비동기로
+    var fli = fs.readdirSync(`./data`, 'utf-8'); // 이건 그냥 시험삼아 Sync로
     var fli_template = template.make_list(fli);
     var control = `<a href="/create">Create post</a>`; //Create는 기본으로 항상 있음
     var ret_template = undefined;
-    // Read Start
+    // ####################### Read Start
     if(pathname=='/'){
         if(queryData.id===undefined){
             title = "Index";
@@ -27,7 +29,7 @@ var app = http.createServer(function(req, res){
             res.end();
         } 
         else{
-            title = queryData.id;
+            title = sani_html(path.parse(queryData.id).base); // 내부 다른 경로의 파일 접근을 막기 위해
             control += `&nbsp;&nbsp;<a href='/update?id=${title}'>Update</a>&nbsp;&nbsp;
             <form action='/delete_process' method='POST'><input type='hidden' name='target' value=${title}><input type='submit' value='Delete'></form>
             `; //Target이 있는 경우 Update, Delete 선택지 추가, Delete는 POST로 처리
@@ -36,7 +38,7 @@ var app = http.createServer(function(req, res){
                     console.log(err);
                     throw err;
                 }
-                description = data;
+                description = sani_html(data);
                 ret_template = template.make_html(title, fli_template, description, control);
                 res.writeHead(200);
                 res.write(ret_template);
@@ -44,8 +46,8 @@ var app = http.createServer(function(req, res){
             });
         }
     }
-    // Read End
-    // Create Start
+    // ####################### Read End
+    // #######################vCreate Start
     else if(pathname=='/create'){
         title = 'Create';
         description = `
@@ -78,7 +80,7 @@ var app = http.createServer(function(req, res){
                 }
                 var post_data = qs.parse(body);
                 console.log(post_data); // post로 받은 값 확인
-                var post_title = post_data.title;
+                var post_title = path.parse(post_data.title).base;
                 var post_des = post_data.description;
                 fs.writeFile(`./data/${post_title}`, post_des, 'utf-8', (err) => {
                     res.writeHead(302, {Location: `/?id=${post_title}`});
@@ -92,20 +94,21 @@ var app = http.createServer(function(req, res){
             res.end();
         }
     }
-    // Create End
-    // Update Start
+    // ####################### reate End
+    // ####################### Update Start
     else if(pathname=='/update'){
-        title = `Updating ${queryData.id}`;
+        var parsed_queryid = path.parse(queryData.id).base;
+        title = `Updating ${parsed_queryid}`;
         control = "Updating...";
-        fs.readFile(`./data/${queryData.id}`, 'utf-8', (err, data) => {
+        fs.readFile(`./data/${parsed_queryid}`, 'utf-8', (err, data) => {
             if(err){
                 console.log(err);
                 throw err;
             }
             description = `
             <form action='/update_process' method='POST'>
-                <input type='hidden' name='origin_title' value=${queryData.id}>
-                Title: <input type='text' name='title' value=${queryData.id}><br>
+                <input type='hidden' name='origin_title' value=${parsed_queryid}>
+                Title: <input type='text' name='title' value=${parsed_queryid}><br>
                 Description:<br>
                 <textarea name='description'>${data}</textarea><br>
                 <input type='submit' value="Submit!">
@@ -133,8 +136,8 @@ var app = http.createServer(function(req, res){
                 }
                 var post_data = qs.parse(body);
                 console.log(post_data); // post로 받은 값 확인
-                var post_origin_title = post_data.origin_title;
-                var post_title = post_data.title;
+                var post_origin_title = path.parse(post_data.origin_title).base;
+                var post_title = path.parse(post_data.title).base;
                 var post_des = post_data.description;
                 fs.rename(`data/${post_origin_title}`, `data/${post_title}`, () => {
                     fs.writeFile(`data/${post_title}`, post_des, 'utf-8', () => {
@@ -150,8 +153,8 @@ var app = http.createServer(function(req, res){
             res.end();
         }
     }
-    // Update End
-    // Delete Start
+    // ####################### Update End
+    // ####################### Delete Start
     else if(pathname=='/delete_process'){
         if(req.method=='POST'){
             var body = "";
@@ -168,7 +171,7 @@ var app = http.createServer(function(req, res){
                 }
                 var post_data = qs.parse(body);
                 console.log(post_data); // post로 받은 값 확인
-                var post_target = post_data.target;
+                var post_target = path.parse(post_data.target).base;
                 fs.unlink(`data/${post_target}`, () => {
                     res.writeHead(302, {Location: '/'});
                     res.end();
@@ -185,7 +188,7 @@ var app = http.createServer(function(req, res){
         res.writeHead(404);
         res.end("404 not found");
     }
-    // Delete End
+    // ####################### Delete End
 });
 
 app.listen(3000, '127.0.0.1');
